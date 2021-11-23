e4efe898e6382140f4f505ac070e46a7274e2721
Visamb <57657892+Visamb@users.noreply.github.com>
2021-11-23 21:55:45 +0100

    Add files via upload
    Removed rounding of y,
    added a new stop criterion

--- nonlinearmin.m
+++ nonlinearmin.m
@@ -2,7 +2,7 @@
 
 x0 = [2;2];
 
-[x,no_its,normgrad] = nonlinearmin(@func,x0,1e-8,'BFGS')
+[x,no_its,normgrad] = nonlinearmin(@func,x0,1e-6,1);
 
 
 %%
@@ -27,43 +27,57 @@ y = x0;
 
 %While stop criterion is not fulfilled
 while criterion == false
-no_its = no_its + 1;
+no_its = no_its + 1
+
 
     for j = 1:n
         
+        
         %Gradient at step y
-        delta_f = grad(@func,y);
+        delta_f = grad(@func,y)
+        
+        test = D
         
         %Direction d
-        dj = -D*delta_f;
+        dj = -D*delta_f
+        
+        %Add this to not get exploding hessians or impossible line search
+        if norm(dj) < 10e-6
+            break
+        end
         
         %Linesearch to find optimal lambda
         F = @(lambda) func(y + dj*lambda);
-        [lambda,nbr,history] = linesearch(F,tol);
+        [lambda,nbr,history] = linesearch(F,1e-32);
         
         %Find new proposal for y
-        y_new = round(y + lambda*dj);
+        y_new = (y + lambda*dj);
         
         %Construct pj and qj 
         pj = lambda*dj;
         qj = grad(@func,y_new)-grad(@func,y);
 
         %Update D with BFGS or DFP depending on chosen method (page 82,89)
-        if method == 'BFGS'
+        if method == 1
              D = D + (1+inv(transpose(pj)*qj)*transpose(qj)*D*qj)*inv(transpose(pj)*qj)*pj*transpose(pj)-inv(transpose(pj)*qj)*(pj*transpose(qj)*D + D*qj*transpose(pj));
-        else
+        elseif method == 0
              D = D + inv(transpose(pj)*qj)*pj*transpose(pj)-inv(transpose(qj)*D*qj)*D*qj*transpose(qj)*D;
         end
         
         %Update y
-        y = y_new;
+        y = y_new
+        
+        %Print 
+        %fprintf('%12.4f %12.4f ',  norm(grad(@func,y)), lambda)
+
+
         
     end
     %When after inner loop, update x
     x = y;
 
 if norm(grad(@func,x)) < tol
-    criterion = true
+    criterion = true;
 end
 
 end
