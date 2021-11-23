3b20ab8c60f4d5f101167f33ff262d7f80e808b8
Visamb <57657892+Visamb@users.noreply.github.com>
2021-11-23 09:27:25 +0100

    Add files via upload

--- nonlinearmin.m
+++ nonlinearmin.m
@@ -2,50 +2,73 @@
 
 x0 = [2;2];
 
-[min,n] = nonlinearmin(@func,x0,1e-8,'BFGS')
+[x,no_its,normgrad] = nonlinearmin(@func,x0,1e-8,'BFGS')
 
 
 %%
 
-function [min, nbr_of_iterations] = nonlinearmin(f,x0,tol,method)
+function [x, no_its, normg] = nonlinearmin(f,x0,tol,method)
 
-x_old = x0;
-y = x_old;
-dim = length(x_old);
-D = eye(dim);
+%Stop criterion
 criterion = false;
-y_new = 0
-nbr_of_iterations = 0;
 
+%Itilialize D
+dim = length(x0);
+D = eye(dim);
+
+%Choose number of iterations for inner loop
+n = 4;
+
+%Initialize number of iterations for outer loop 
+no_its = 0;
+
+%First y (for inner loop)
+y = x0;
+
+%While stop criterion is not fulfilled
 while criterion == false
-nbr_of_iterations = nbr_of_iterations+1;
+no_its = no_its + 1;
 
-for j = 1:10
-delta_f = grad(@func,y);
-dj = -D*delta_f
-F = @(lambda) func(y + dj*lambda);
-[lambda,nbr,history] = linesearch(F,tol);
-y_new = round(y + lambda*dj);
-pj = lambda*dj;
-qj = grad(@func,y_new)-grad(@func,y);
+    for j = 1:n
+        
+        %Gradient at step y
+        delta_f = grad(@func,y);
+        
+        %Direction d
+        dj = -D*delta_f;
+        
+        %Linesearch to find optimal lambda
+        F = @(lambda) func(y + dj*lambda);
+        [lambda,nbr,history] = linesearch(F,tol);
+        
+        %Find new proposal for y
+        y_new = round(y + lambda*dj);
+        
+        %Construct pj and qj 
+        pj = lambda*dj;
+        qj = grad(@func,y_new)-grad(@func,y);
 
-if method == 'BFGS'
-D = D + (1+inv(transpose(pj)*qj)*transpose(qj)*D*qj)*inv(transpose(pj)*qj)*pj*transpose(pj)-inv(transpose(pj)*qj)*(pj*transpose(qj)*D + D*qj*transpose(pj));
-else
-D = D + inv(transpose(pj)*qj)*pj*transpose(pj)-inv(transpose(qj)*D*qj)*D*qj*transpose(qj)*D;
-end
-y = y_new
-end
+        %Update D with BFGS or DFP depending on chosen method (page 82,89)
+        if method == 'BFGS'
+             D = D + (1+inv(transpose(pj)*qj)*transpose(qj)*D*qj)*inv(transpose(pj)*qj)*pj*transpose(pj)-inv(transpose(pj)*qj)*(pj*transpose(qj)*D + D*qj*transpose(pj));
+        else
+             D = D + inv(transpose(pj)*qj)*pj*transpose(pj)-inv(transpose(qj)*D*qj)*D*qj*transpose(qj)*D;
+        end
+        
+        %Update y
+        y = y_new;
+        
+    end
+    %When after inner loop, update x
+    x = y;
 
-x_old = y;
-
-if norm(grad(@func,x_old)) < tol
+if norm(grad(@func,x)) < tol
     criterion = true
 end
 
 end
 
-min = x_old
+normg = grad(@func,x);
 
 end
 
